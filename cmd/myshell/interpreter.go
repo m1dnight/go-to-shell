5b37e0ec7decd7d3e89e8513bf7368afadf3c92a
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-06-30 16:54:21 +0200

    stage 5 done

--- cmd/myshell/interpreter.go
+++ cmd/myshell/interpreter.go
@@ -4,9 +4,10 @@ import (
 	"os"
 	"slices"
 	"strconv"
+	"strings"
 )
 
-var commands = []string{"exit"}
+var commands = []string{"exit", "echo"}
 
 func evaluate(command command) (*successResult, *errorResult) {
 	if !slices.Contains(commands, command.cmd) {
@@ -14,20 +15,31 @@ func evaluate(command command) (*successResult, *errorResult) {
 		return nil, &result
 	}
 
-	//var success successResult
-	var errResult errorResult
+	var sucResult *successResult = nil
+	var errResult *errorResult = nil
 	switch command.cmd {
 	case "exit":
+		if len(command.args) != 1 {
+			errResult = &errorResult{message: "to few arguments", cmd: command.cmd}
+			return sucResult, errResult
+		}
 		exitCode := command.args[0]
 		exitCodeNbr, err := strconv.Atoi(exitCode)
 		if err != nil {
-			errResult = errorResult{message: "invalid argument to command", cmd: command.cmd}
+			errResult = &errorResult{message: "invalid argument to command", cmd: command.cmd}
+			return sucResult, errResult
 		} else {
 			os.Exit(exitCodeNbr)
 		}
+	case "echo":
+		outputMessage := strings.Join(command.args, " ")
+		sucResult = &successResult{message: outputMessage}
+		return sucResult, errResult
+
 	default:
-		errResult = errorResult{message: "unknown error", cmd: command.cmd}
+		errResult = &errorResult{message: "unknown error", cmd: command.cmd}
+		return sucResult, errResult
 
 	}
-	return nil, &errResult
+	return sucResult, errResult
 }
